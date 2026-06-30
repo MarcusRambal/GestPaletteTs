41006307782de86d0a694564f6a592a4c804f285
Marcus <marcusrambalpreston@gmail.com>
2026-06-30 16:01:01 -0500

    feat: new View (EditDelete) and implement SQLite queries for creating products and categories

---
+++ src/components/EditDelete/EditDelete.ts
@@ -0,0 +1,14 @@
+
+
+export class EditDelete {
+
+    render(container: HTMLElement) {
+
+    container.innerHTML = `
+      <div class="editDelete-container"> 
+         
+      </div>
+    `;
+
+    }
+}
